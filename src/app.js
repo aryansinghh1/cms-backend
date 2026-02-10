55e096ad4a4ab40a3b7fdf8dfa12ce009c086d70
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-10 09:52:25 +0530

    app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(express.json({limit: "10mb"}));
 app.use(morgan("dev"));
 
+
 app.get("/",(req,res)=> res.send("CMS API Running"));
 
 app.get("/api/auth",require("./routes/auth.routes"));
