cdf20e2359ffe861d80f516d7de557a456f860b4
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-10 09:53:41 +0530

    creating server

--- src/app.js
+++ src/app.js
@@ -8,10 +8,9 @@ app.use(cors());
 app.use(express.json({limit: "10mb"}));
 app.use(morgan("dev"));
 
-
 app.get("/",(req,res)=> res.send("CMS API Running"));
 
-app.get("/api/auth",require("./routes/auth.routes"));
+app.use("/api/auth",require("./routes/auth.routes"));
 app.use("/api/artifact",require("./routes/artifact.routes"));
 
 module.exports = app;
