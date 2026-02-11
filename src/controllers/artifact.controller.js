e3e2aa7b5e9652ca69921852f3645fb9493a83ee
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-11 09:42:34 +0530

    adding likes and comments features

--- src/controllers/artifact.controller.js
+++ src/controllers/artifact.controller.js
@@ -1,14 +1,96 @@
 const Artifact = require("../models/artifact.model");
 
-exports.createArtifact = async (req,res) => {
-    const artifact = await Artifact.create({
-        ...req.body,
-        createdBy: req.user.id,
-    });
-    res.json(artifact);
+exports.createArtifact = async (req, res) => {
+  const artifact = await Artifact.create({
+    ...req.body,
+    createdBy: req.user.id,
+  });
+  res.json(artifact);
 };
 
 exports.getArtifacts = async (req, res) => {
-    const artifacts = await Artifact.find().populate("createdBy","email");
-    res.json(artifacts);
-}
+  const artifacts = await Artifact.find().populate("createdBy", "email");
+  res.json(artifacts);
+};
+
+exports.toggleLike = async (req, res) => {
+  const { id } = req.params; // artifact id
+  const userId = req.user.id;
+
+  const artifact = await Artifact.findById(id);
+  if (!artifact) return res.status(404).json({ message: "Artifact not found" });
+
+  const alreadyLiked = artifact.likes.includes(userId);
+
+  if (alreadyLiked) {
+    // Unlike
+    artifact.likes = artifact.likes.filter(
+      (like) => like.toString() !== userId,
+    );
+  } else {
+    // Like
+    artifact.likes.push(userId);
+  }
+
+  await artifact.save();
+
+  res.json({
+    message: alreadyLiked ? "Unliked" : "Liked",
+    totalLikes: artifact.likes.length,
+  });
+};
+
+exports.getLikes = async (req, res) => {
+  const { id } = req.params;
+
+  const artifact = await Artifact.findById(id).populate("likes", "email");
+
+  if (!artifact) return res.status(404).json({ message: "Artifact not found" });
+
+  res.json({
+    totalLikes: artifact.likes.length,
+    users: artifact.likes,
+  });
+};
+
+
+exports.addComment = async (req, res) => {
+  const { id } = req.params; // artifact id
+  const { text } = req.body;
+  const userId = req.user.id;
+
+  if (!text)
+    return res.status(400).json({ message: "Comment text required" });
+
+  const artifact = await Artifact.findById(id);
+  if (!artifact)
+    return res.status(404).json({ message: "Artifact not found" });
+
+  artifact.comments.push({
+    user: userId,
+    text,
+  });
+
+  await artifact.save();
+
+  res.json({
+    message: "Comment added",
+    totalComments: artifact.comments.length,
+  });
+};
+
+
+exports.getComments = async (req, res) => {
+  const { id } = req.params;
+
+  const artifact = await Artifact.findById(id)
+    .populate("comments.user", "email");
+
+  if (!artifact)
+    return res.status(404).json({ message: "Artifact not found" });
+
+  res.json({
+    totalComments: artifact.comments.length,
+    comments: artifact.comments,
+  });
+};
--- src/models/artifact.model.js
+++ src/models/artifact.model.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose");
 
-
-const artifactSchema = new mongoose.Schema(
-    {
-        title: String,
-        description: String,
-        createdBy: {type:mongoose.Schema.Types.ObjectId,ref: "User"},
-    },
-    {
-        timestamps:true
-    },
+const commentSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    text: String,
+  },
+  { timestamps: true },
 );
 
-module.exports = mongoose.model("Artifact",artifactSchema);
+const artifactSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [commentSchema],
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Artifact", artifactSchema);
